65d6a0386e7b4a4f997978b637873860daad15fc
MaelRD <marioyaelgg@gmail.com>
2025-03-26 07:54:15 -0600

    Actualizacion 4.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'static', // Asegúrate de que sea un sitio estático
   base: '/portafolio', // Nombre del repositorio en GitHub
